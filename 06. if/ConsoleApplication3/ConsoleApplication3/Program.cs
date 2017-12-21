9639778ad7a51600958dbcebc08579a5acd5f1b6
Pehov <34743440+Pehov@users.noreply.github.com>
2017-12-21 18:11:36 +0300

    06

---
+++ 06. if/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("рост");
+            int rost = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("твой вес");
+            int ves = int.Parse(Console.ReadLine());
+            
+            if (rost - 100 == ves) {
+                Console.WriteLine("норма");
+            }
+            if (rost - 100 < ves) {
+                Console.WriteLine("толстей");
+            }
+            if (rost - 100 > ves) {
+                Console.WriteLine("худей");
+            }
+            Console.ReadLine();
+        }
+    }
+}
