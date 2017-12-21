d1ab64e6328f009ed9ae8c3c42d56778bf1f6887
Pehov <34743440+Pehov@users.noreply.github.com>
2017-12-21 18:17:02 +0300

    06 драконы

---
+++ 06. if/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("возраст дракона");
+            int are = int.Parse(Console.ReadLine());
+        
+            Console.WriteLine("здоровье дракона");
+            int hp = int.Parse(Console.ReadLine());
+
+            if (are < 150)
+            {
+                Console.WriteLine("Бейся");
+            }
+            if (are > 150)
+            {
+                Console.WriteLine("уходи");
+            }
+            if (hp < 50)
+            {
+                Console.WriteLine("Бейся");
+            }
+            if (hp < 50)
+            {
+                Console.WriteLine("уходи");
+            }
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
