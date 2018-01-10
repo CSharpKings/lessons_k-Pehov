073d893da838638f65a28be36c2b77ca91825fbb
Alex <iseldomused@gmail.com>
2018-01-10 22:16:31 +0300

    add 08

---
+++ 08. while/CheckPassword/CheckPassword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPassword {
+	class Program {
+		static void Main(string[] args) {
+			string password = "Супер пароль!";
+			Console.WriteLine("Чтобы пользоваться моей программой, введи пароль!");
+			string answer = Console.ReadLine();
+			
+			if (answer == password) {
+				Console.WriteLine("Ок, правильно. Жми Enter, чтобы пройти!");
+				Console.ReadLine();
+				Console.Clear();
+
+				StartProgram();
+			}
+			else {
+				Console.WriteLine("Неправильно! Хаха, ты не пройдешь!");
+				Console.ReadLine();
+				Environment.Exit(0);
+			}
+		}
+
+		static void StartProgram() {
+			Quest quest = new Quest();
+		}
+	}
+}
---
+++ 08. while/Magnitude/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magnitude {
+class Program {
+static void Main(string[] args) {
+int num;
+Console.WriteLine("Введите число:");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine("Число: " + num);
+
+Console.WriteLine("Длина числа: " + GetMagnitude(num));
+Console.Read();
+}
+
+static int GetMagnitude(int number) {
+int mag = 0;
+while (number > 0) {
+mag++;
+number = number / 10;
+}
+
+return mag;
+}
+}
+}
