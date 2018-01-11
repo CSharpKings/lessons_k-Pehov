01eefb98cfebff333c6b81ae83dfda1d92d23f87
Pehov <34743440+Pehov@users.noreply.github.com>
2018-01-11 18:14:32 +0300

    Цыкл

--- 08. while/Magnitude/Program.cs
+++ 08. while/Magnitude/Program.cs
@@ -5,25 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Magnitude {
-class Program {
-static void Main(string[] args) {
-int num;
-Console.WriteLine("Введите число:");
-num = int.Parse(Console.ReadLine());
-Console.WriteLine("Число: " + num);
+    class Program {
+        static void Main(string[] args) {
+            int num;
+            Console.WriteLine("Введите число:");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Число: " + num);
 
-Console.WriteLine("Длина числа: " + GetMagnitude(num));
-Console.Read();
-}
+            Console.WriteLine("Длина числа: " + GetMagnitude(num));
+            Console.Read();
+        }
 
-static int GetMagnitude(int number) {
-int mag = 0;
-while (number > 0) {
-mag++;
-number = number / 10;
-}
+        static int GetMagnitude(int number) {
+            int mag = 0;
+            while (number > 0) {
+                mag++;
+                number = number / 10;
+            }
 
-return mag;
-}
-}
+            return mag;
+        }
+    }
 }
---
+++ cem_8_2/cem_8_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace cem_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 0;
+            while (true)
+            {
+                Console.WriteLine("0" + time);
+                Thread.Sleep(1000);
+                time = time + 1;
+            }
+        }
+    }
+}
