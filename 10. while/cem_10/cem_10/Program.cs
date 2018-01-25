c400bac7c99162411bdad27aad826230e3ee02bf
Pehov <34743440+Pehov@users.noreply.github.com>
2018-01-25 18:03:38 +0300

    вывод квадратов числа

---
+++ 10. while/cem_10/cem_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cem_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            int nomber = 0;
+            Console.WriteLine("квадраты не превосходящие " + n);
+            while (true)
+            {
+                nomber = nomber + 1;
+                int quadrare = nomber * nomber;
+                if (quadrare > n)
+                {
+                    Console.ReadLine();
+                    break;
+                }
+                Console.WriteLine(quadrare);
+            }
+        }
+    }
+}
