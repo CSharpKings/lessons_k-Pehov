343c2a7f8026d5a7c5da6db575c6c820d1558046
Pehov <34743440+Pehov@users.noreply.github.com>
2018-02-15 17:59:49 +0300

    Cem_12

---
+++ 12. for/Cem_12/Cem_12_3/Cem_12_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cem_12_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 10; number <= 25; number++)
+            {
+                Console.WriteLine(number + " "+ number + ".4");
+            }
+            Console.ReadLine();
+        }
+    }
+}
