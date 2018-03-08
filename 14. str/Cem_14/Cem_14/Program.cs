ce6234f7f3c314ae6a4df17dc31580456e11466b
Pehov <34743440+Pehov@users.noreply.github.com>
2018-03-08 18:09:43 +0300

    sem_14

---
+++ 14. str/Cem_14/Cem_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cem_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово.");
+            string str = Console.ReadLine();
+            int strlight = str.Length;
+            for (int i = 0; i < str.Length; i++)
+            {
+                int contrarilyI = 0;
+                contrarilyI = str.Length - i - 1;
+                Console.WriteLine(str[contrarilyI]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
