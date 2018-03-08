09d61cb93f6b7c8e9da0e4b084050335502963d5
Pehov <34743440+Pehov@users.noreply.github.com>
2018-03-08 18:26:24 +0300

    str, masiv

---
+++ 14. str/sem_14_masiv/sem_14_masiv/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sem_14_masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, 6, 5, 2, 5};
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (numbers[i] == 5)
+                {
+                    Console.WriteLine("номер '5' "+ i);
+                    break;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
