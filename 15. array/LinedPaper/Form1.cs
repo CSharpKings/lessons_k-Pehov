da44e3ea21e5ed875fabfec2a5a34dc098fba203
Alex <iseldomused@gmail.com>
2018-03-29 13:45:52 +0300

    add 15

---
+++ 15. array/LinedPaper/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinedPaper {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		protected override void OnPaint(PaintEventArgs e) {
+			Graphics graphics = e.Graphics;
+			graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+			for (int i = 0; i <= 150; i += 20) {
+				graphics.DrawLine(Pens.LightSkyBlue, 0, i, 300, i); // первая пара чисел - координаты начала отрезка, вторая - координаты конца
+			}
+		}
+	}
+}
