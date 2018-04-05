45b18bf9848069390c15537b734c4a4d43f96fe6
Pehov <34743440+Pehov@users.noreply.github.com>
2018-04-05 18:23:43 +0300

    dz2_15

---
+++ 15. array/DZ2_15/DZ2_15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DZ2_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, -5, 3, 5, -19, -15, 7, 8};
+            for (int i = 1; i < numbers.Length - 1; i++) 
+            {
+                if (numbers[i] < 0 && numbers[i + 1] < 0 || numbers[i] > 0 && numbers[i + 1] > 0)
+                {
+                        Console.WriteLine(numbers[i] + " " + numbers[i+1]);
+                        break;
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 15. array/DZ2_15_5/DZ2_15_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DZ2_15_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            int[] numbers = new int[12];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = gen.Next(163,190);
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
