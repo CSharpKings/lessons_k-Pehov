a2ffb4acdb7ae8c9e82b6b2199d3cb78b5aedd34
Pehov <34743440+Pehov@users.noreply.github.com>
2018-04-12 18:01:32 +0300

    DZ_2_15_6

---
+++ 15. array/DZ2_15_6/DZ2_15_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DZ2_15_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int[] numbers = new int[17];
+            for (int i = 60; i >= 12; i = i - 3)
+            {
+                numbers[n] = i;
+                n = n + 1;
+            }
+            for (int n2 = 0; n2 < numbers.Length; n2++)
+            {
+                Console.WriteLine(numbers[n2]);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
